936935e5669dd64a0eb8a233fdf6d2f53adf2a9e
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-29 13:51:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-
+from setuptools import setup
+setup(
+    name = 'serverlist_CLI',
+    version = '0.1.0',
+    packages = ['serverlist'],
+    entry_points = {
+        'console_scripts': [
+            'serverlist = serverlist.__main__:main'
+        ]
+    })
