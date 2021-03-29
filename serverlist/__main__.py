180ad3dd270e14df9cbc775008d98952987f6ec0
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-29 22:17:47 +0100

    Update __main__.py

--- serverlist/__main__.py
+++ serverlist/__main__.py
@@ -6,24 +6,48 @@ def main():
     if len(args) == 0:
         print('You must have an argument')
         return
+    
+    
     serverlist =requests.get('https://servers.minetest.net/list')
     serverlistinfo = serverlist.text
     jsonserverlistinfo = json.loads(serverlistinfo)
-    #print(serverlistinfo)
-    for arg in args:
+    
+    
+    if len(args) == 1:
+        arg = sys.argv[1]
         if arg == 'total_clients':
             total = jsonserverlistinfo['total']
             print(total['clients'])
+            
         if arg == 'total_servers':
             total = jsonserverlistinfo['total']
             print(total['servers'])
+        
         if arg == 'total_max_clients':
             total_max = jsonserverlistinfo['total_max']
             print(total_max['clients'])
+            
         if arg == 'total_max_servers':
             total_max = jsonserverlistinfo['total_max']
             print(total_max['servers'])
+        
         if arg == 'serverlist':
             print(serverlistinfo)
+            
+    #for arg in args:
+        #if arg == 'total_clients':
+        #    total = jsonserverlistinfo['total']
+        #   print(total['clients'])
+        #if arg == 'total_servers':
+        #    total = jsonserverlistinfo['total']
+        #    print(total['servers'])
+        #if arg == 'total_max_clients':
+        #    total_max = jsonserverlistinfo['total_max']
+        #    print(total_max['clients'])
+        #if arg == 'total_max_servers':
+        #    total_max = jsonserverlistinfo['total_max']
+        #    print(total_max['servers'])
+        #if arg == 'serverlist':
+        #    print(serverlistinfo)
 if __name__ == '__main__':
     main()
