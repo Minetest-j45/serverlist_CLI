87056899aa65c445a177d1777e3b22f0244d0698
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-29 14:15:35 +0100

    Update __main__.py

--- serverlist/__main__.py
+++ serverlist/__main__.py
@@ -2,8 +2,11 @@ import sys
 def main():
     print('in main')
     args = sys.argv[1:]
+    if len(args) < 0:
+        return
     print('count of args :: {}'.format(len(args)))
     for arg in args:
         print('passed argument :: {}'.format(arg))
+    if 
 if __name__ == '__main__':
     main()
