c3097453ca50b8000e5aa78f4c4d20e262e1dbd0
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-29 14:45:55 +0100

    Update __main__.py

--- serverlist/__main__.py
+++ serverlist/__main__.py
@@ -11,11 +11,17 @@ def main():
     jsonserverlistinfo = json.loads(serverlistinfo)
     #print(serverlistinfo)
     for arg in args:
-        if arg == 'totalclients':
+        if arg == 'total_clients':
             total = jsonserverlistinfo['total']
             print(total['clients'])
-        if arg == 'totalservers':
+        if arg == 'total_servers':
             total = jsonserverlistinfo['total']
             print(total['servers'])
+         if arg == 'total_max_clients':
+            total_max = jsonserverlistinfo['total_max']
+            print(total_max['clients'])
+        if arg == 'total_max_servers':
+            total_max = jsonserverlistinfo['total_max']
+            print(total_max['servers'])
 if __name__ == '__main__':
     main()
